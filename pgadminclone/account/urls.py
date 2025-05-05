0f78545823b67c6ef7c4f1106472c83ce14d5a73
Suraj kr. Gupta <64039192+surajsohigh@users.noreply.github.com>
2025-05-06 02:17:07 +0530

    Update urls.py

--- pgadminclone/account/urls.py
+++ pgadminclone/account/urls.py
@@ -5,7 +5,6 @@ from .views import *
 
 urlpatterns = [
     path('user/', UserView.as_view(), name='register'),
-    path('user/<int:pk>/', UserView.as_view(), name='userDetails'),
     path('login/', LoginView.as_view(), name='login'),
     path('userprofile/', UserProfile.as_view(), name='profile'),
     path("logout/", LogoutView.as_view(), name="logout")
